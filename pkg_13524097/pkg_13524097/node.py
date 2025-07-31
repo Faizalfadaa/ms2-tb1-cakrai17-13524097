caa673d1d27e570bc557b41f8c3058dc7547852f
Faizalfadaa <faizalfadha@gmail.com>
2025-07-31 13:43:16 +0700

    refactor: Added Bonus 2 to code (raaahh)

--- pkg_13524097/pkg_13524097/node.py
+++ pkg_13524097/pkg_13524097/node.py
@@ -4,6 +4,9 @@ import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
 from std_msgs.msg import String
+import threading
+from queue import PriorityQueue
+
 
 class NodeTemplate(Node):
     def __init__(self):
@@ -19,6 +22,10 @@ class NodeTemplate(Node):
         self.declare_parameter('autonomous_vel_topic', 'autonomous_vel') # Subscriber
         self.declare_parameter('publish_rate', 5.0)                      # 5 message / second (5 Hz)
 
+        # Spesifikasi Bonus 2
+        self.declare_parameter('joy_vel_topic', 'joy_vel')
+        self.declare_parameter('keyboard_vel_topic', 'keyboard_vel')
+
         # ==============================================
         # PARAMETER RETRIEVAL
         # ==============================================
@@ -29,6 +36,12 @@ class NodeTemplate(Node):
         autonomous_vel_topic = self.get_parameter('autonomous_vel_topic').get_parameter_value().string_value
         publish_rate = self.get_parameter('publish_rate').get_parameter_value().double_value 
         # self.get_logger().info(f'Publish rate: {publish_rate}')
+
+        # Spesifikasi Bonus 2
+        joy_vel_topic = self.get_parameter('joy_vel_topic').get_parameter_value().string_value
+        keyboard_vel_topic = self.get_parameter('keyboard_vel_topic').get_parameter_value().string_value
+        self.lock = threading.Lock()
+        self.pq = PriorityQueue() 
 
         # ==============================================
         # PUBLISHERS
@@ -60,6 +73,23 @@ class NodeTemplate(Node):
             self.autonomous_vel_callback,  # Callback function
             10                             # Queue size
         )
+
+        # Spesifikasi Bonus 2
+        # 2. Subscriber joy_vel_topic
+        self.joy_vel_sub = self.create_subscription(
+            Twist,
+            joy_vel_topic,
+            self.joy_vel_callback,
+            10
+
+        )
+        # 3. Subscriber keyboard_vel_topic
+        self.keyboard_vel_sub = self.create_subscription(
+            Twist,
+            keyboard_vel_topic,
+            self.keyboard_vel_callback,
+            10
+        )
        
         # ==============================================
         # TIMER
@@ -81,8 +111,22 @@ class NodeTemplate(Node):
     # Create Subscriber Callback Function
     def autonomous_vel_callback(self, msg):
         """Callback for autonomous_vel messages"""
-        self.current_autonomous_vel = msg
-        self.current_cmd_type = "autonomous"
+        with self.lock:
+            self.current_cmd_vel = msg
+            self.current_cmd_type = "autonomous"
+    
+    # Spesifikasi Bonus 2
+    def joy_vel_callback(self, msg):
+        """Callback for joy_vel messages"""
+        with self.lock:
+            self.current_cmd_vel = msg
+            self.current_cmd_type = "joy"
+    
+    def keyboard_vel_callback(self, msg):
+        """Callback for keyboard_vel messages"""
+        with self.lock:
+            self.current_cmd_vel = msg
+            self.current_cmd_type = "keyboard"
    
     # ==================================================
     # TIMER CALLBACK
@@ -91,10 +135,21 @@ class NodeTemplate(Node):
     # Create Timer Callabck Function
     def timer_callback(self):
         """Timer callback for publish"""
-        msg = String()
-        msg.data = self.current_cmd_type
-        self.cmd_type_pub.publish(msg)
-        self.cmd_vel_pub.publish(self.current_autonomous_vel)
+        msg_type = ["keyboard", "joy", "autonomous"]
+
+        if self.current_cmd_type == msg_type[0]:
+            self.pq.put((1, msg_type[0]))
+        elif self.current_cmd_type == msg_type[1]:
+            self.pq.put((2, msg_type[1]))
+        elif self.current_cmd_type == msg_type[2]:
+            self.pq.put((3, msg_type[2]))
+
+        with self.lock:
+            self.current_cmd_type = self.pq.get()[1]
+            msg = String()
+            msg.data = self.current_cmd_type
+            self.cmd_type_pub.publish(msg)
+            self.cmd_vel_pub.publish(self.current_cmd_vel)
 
 def main(args=None):
     rclpy.init(args=args)
